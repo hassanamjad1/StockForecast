6743c5a43cc7bd92acea5af9fe2e2ee421e28c8b
hassanamjad1 <haxxter1977@gmail.com>
2021-08-05 13:07:01 -0400

    loading/preparing data

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ import pandas as pd
 # from pandas_datareader  import data
 import pandas_datareader.data as web
 import datetime as dt
+
+
+
+
+# loading data
+company = 'TSLA'
+print(np)
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2020,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+# prepare date
+scaler = MinMaxScaler(feature_range = (0,1))
