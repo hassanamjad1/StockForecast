80da9783625cc65aedc279190f0f9cabee5e32b0
hassanamjad1 <haxxter1977@gmail.com>
2021-08-05 13:09:42 -0400

    testing and plotting

--- main.py
+++ main.py
@@ -62,3 +62,41 @@ model.compile(optimizer = 'adam', loss = 'mean_squared_err' )
 
 #fitting the model on trainng data
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+
+#-- Testing the model I just built --
+
+#Loading the data from after the end date which model has never seem
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
+
+test_data =  web.DataReader(company, 'yahoo', test_start,test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis = 0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.transform(model_inputs)
+
+#Make Predicting on Test Data
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days: x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test)
+predicted_prices = scaler.inverse_transform(predicted_prices)
+
+#Plotting the test preditions
+plt.plot(actual_prices, color="black", label= f"Actual {company} Price")
+plt.plot(predicted_prices, color='green', label = f"Predicted {company} Price")
+plt.title(f"{company} Share Price")
+plt.xlabel('Time')
+plt.ylabel(f'{company} Share Price')
+plt.legend()
+plt.show()
